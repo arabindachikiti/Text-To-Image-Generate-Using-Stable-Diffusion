6c1f8a22ef46320b83bb1509b981dd9bbd368b4f
Arabinda Panigrahi <53396452+arabindachikiti@users.noreply.github.com>
2025-04-23 21:40:41 +0530

    Update Text_To_Image_Stable_Diffusion.ipynb.py

--- Text_To_Image_Stable_Diffusion.ipynb.py
+++ Text_To_Image_Stable_Diffusion.ipynb.py
@@ -1,1 +1,39 @@
-print('Hello from Colab!')
+pip install diffusers --upgrade
+
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+import torch
+
+model_id = "stabilityai/stable-diffusion-2-1-base"
+
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+prompt = "beautiful greeenery forest image with birds"
+image = pipe(prompt).images[0]
+
+image.save("astronaut_rides_horse.png")
+
+pip install gradio
+
+import gradio as gr
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+import torch
+
+model_id = "stabilityai/stable-diffusion-2-1-base"
+
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+def generated_image(prompt):
+    image = pipe(prompt).images[0]
+    return image
+
+gr.Interface(
+    fn=generated_image,
+    inputs=gr.Textbox(label="Enter your prompt"),
+    outputs=gr.Image(type="pil"),
+    title="Stable Diffusion 2.1 - ARABINDA PANIGRAHI IMAGE GENERATOR APP",
+    description="Enter text prompt and see the generated image"
+).launch()
